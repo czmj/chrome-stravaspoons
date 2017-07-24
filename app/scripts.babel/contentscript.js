d441c45f5c174ca947f144ab55df7b2c7f3b3862
czmj <cj@theblackeyeproject.co.uk>
2017-07-24 14:41:27 +0100

    print array of pubs

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,38 @@
 'use strict';
 
-console.log('Content script');
+function weatherspoonsApiRequest(options) {
+    if (!options) {
+        options = { 'region': null, 'paging': {'UsePagination': false}, 'facilities': [], 'searchType': 0 };
+    }
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', 'https://www.jdwetherspoon.com/api/advancedsearch', true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+    xhttp.send(JSON.stringify(options));
+    xhttp.onload = weatherspoonsApiRequestSuccess;
+    xhttp.onerror = weatherspoonsApiRequestError;
+}
+
+function weatherspoonsApiRequestSuccess() { 
+    var response = JSON.parse(this.responseText)
+    console.log(collectPubs(response.regions));
+}
+
+function weatherspoonsApiRequestError() { 
+    console.error(this.statusText); 
+}
+
+function collectPubs(regions) {
+    var pubs = [];
+    regions.forEach(region => {
+        region.subRegions.forEach(subregion => {
+            subregion.items.forEach(pub => {
+                if (!(pub.PubIsTemporaryClosed || pub.PubIsClosed))
+                    pubs.push(pub)
+            })
+        })
+    })
+    return pubs;
+}
+
+weatherspoonsApiRequest();
